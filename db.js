9f8fb8bd4426cc3f964e5c971291eee3c97c5a7d
rpardo440828 <70660970+rpardo440828@users.noreply.github.com>
2024-02-04 11:35:40 -0500

    Final fixed database

--- db.js
+++ db.js
@@ -1,13 +1,15 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-    host:"mysql-app-database:3306",
-    user:"mysql",
-    password:"@Wildcats2019",
-    database:"my_database"
+    host:"us-cluster-east-01.k8s.cleardb.net",
+    user:"b00a11bcd68c93",
+    password:"60e69693",
+    database:"heroku_f06e8b7c58d386f"
 
     // host:"localhost",
     // user:"root",
     // password:"@Wildcats2019",
     // database:"sqlproj"
 });
+
+// mysql://b00a11bcd68c93:60e69693@us-cluster-east-01.k8s.cleardb.net/heroku_f06e8b7c58d386f?reconnect=true
